27a6e651829f068f417692c7a8e8ddd79552165b
Patricio Vargas <pevargas@valdosta.edu>
2016-10-18 20:49:47 -0400

    delete and update

--- routes/tasks.js
+++ routes/tasks.js
@@ -48,4 +48,29 @@ router.delete('/task/:id',function(req, res, next){
         res.json(task);
     });
 });
+//Update Task
+router.put('/task/:id',function(req, res, next){
+    var task = req.body;
+    var updTask = {};
+    if(task.isDone){
+        updTask.isDone = task.isDone;
+    }
+    if(task.title){
+        updTask.title = task.title;
+    }
+    if(!updTask){
+        res.status(400);
+        res.json({
+            "error" : "Bad Data"
+        });
+    }
+    else{
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask,{},function(err,task){
+            if(err){
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }
+});
 module.exports = router;
